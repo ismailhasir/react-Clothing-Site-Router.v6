85d51e1e3a5ff92ed579587519080604dbc29346
İsmail HASIR <ismailhasir@gmail.com>
2022-03-06 14:03:17 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Products from './pages/Products'
+import ProductDetails from './pages/ProductDetails'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <nav>
+          <h1>The Ninja Clothing Company</h1>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/products/:id">
+            <ProductDetails />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
